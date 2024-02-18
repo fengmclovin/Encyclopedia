a0efb1ad22bce2703db590c141087ddf07f8a3b5
Lance Tian <79405882+fengmclovin@users.noreply.github.com>
2024-02-19 00:18:35 +1100

    Update views.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+import markdown
+
 from . import util
 
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
