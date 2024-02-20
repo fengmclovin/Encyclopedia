160d123d3d5c62736974f741bb7aeb9cdb1e9d92
Lance Tian <79405882+fengmclovin@users.noreply.github.com>
2024-02-21 01:14:52 +1100

    Update urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
-    path("new/", views.new_page, name="new_page")
+    path("newpage/", views.new_page, name="new_page"),
+    path("random/", views.random_page, name="random_page"),
+    path("edit/", views.edit, name="edit"),
+    path("save_edit/", views.save_edit, name="save_edit")
 ]
